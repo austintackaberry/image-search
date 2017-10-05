fb4cb68759d3d010c90ffaca7e39223d48de73be
Austin Tackaberry <austintackaberry@gmail.com>
2017-10-05 01:30:13 -0700

    got img titles from imgur api, added img tags to db, made API to get arr of rand imgs

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-var http = require('http');
+var http = require('https');
 var express = require('express');
-var request = require('request');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var URL = 'mongodb://austin:mongodb@ds157584.mlab.com:57584/austintackaberry'
 var db;
 var async = require('async');
-// app.use('/', express.static('public'));
 
 MongoClient.connect(URL, (err, database) => {
   if (err) return console.log(err)
   db = database;
-  app.listen(3000);
-  console.log('listening on 3000')
+  app.listen(4000);
+  console.log('listening on 4000')
 });
 
-
-var pathArr = [];
-var q = async.queue(function (task, done) {
-  request('http://www.imgur.com/random', function (error, response, body) {
-    var parsed = body.match(/(?:","title":")(.*?)(?:","score":)/g);
-    if (parsed !== null) {
-      var parsedStr = parsed[0].split(/(?:","title":")|(?:","score":)/)[1];
-    }
-    var pathName = response.request.uri.path.split('/');
-    pathName = pathName[pathName.length-1];
-    if (parsed !== null) {
-      if (parsedStr.length < 150) {
-        var pathObj = {};
-        pathObj["title"] = parsedStr;
-        pathObj["pathName"] = pathName;
-        db.collection("imgur").update({pathName: pathName}, pathObj, {upsert:true});
+app.get('/:num', function(req,res) {
+  console.log("request received");
+  var listImg = [];
+  var allImg = [];
+  async.series([
+    function(callback) {
+      db.collection("imgur").find().toArray()
+      .then(function(result) {
+        allImg = result;
+        callback();
+      })
+    },
+    function(callback) {
+      var randoList = [];
+      for (var i = 0; i < req.params.num; i++) {
+        while (true) {
+          var rando = Math.floor(Math.random() * allImg.length);
+          if (!randoList.includes(rando)) {
+            randoList.push(rando);
+            break;
+          }
+        }
+        console.log(rando);
+        listImg.push(allImg[rando]);
       }
+      callback();
+    },
+    function(callback) {
+      res.send(listImg);
+      callback();
     }
-    done();
-  });
-});
-
-for (var i = 0; i < 500; i++) {
-  q.push({index: i});
-}
-
-app.get('/', function(req,res) {
-  res.send(pathArr);
+  ]);
 });
---
+++ appdb.js
@@ -0,0 +1,79 @@
+var http = require('https');
+var express = require('express');
+var request = require('request');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var URL = 'mongodb://austin:mongodb@ds157584.mlab.com:57584/austintackaberry'
+var db;
+var async = require('async');
+
+MongoClient.connect(URL, (err, database) => {
+  if (err) return console.log(err)
+  db = database;
+  app.listen(3000);
+  console.log('listening on 3000')
+});
+
+var pathArr = [];
+var q = async.queue(function (task, done) {
+  var pathName;
+  var title;
+  var isSevenChar = false;
+  async.series([
+    function(callback) {
+      request('http://www.imgur.com/random', function (error, response, body) {
+        pathName = response.request.uri.path.split('/');
+        pathName = pathName[pathName.length-1];
+        if (pathName.length === 7) {
+          isSevenChar = true;
+        }
+        callback();
+      });
+    },
+    function(callback) {
+      var options = {
+        "method": "GET",
+        "hostname": "api.imgur.com",
+        "path": "/3/gallery/image/" + pathName,
+        "headers": {
+          "Authorization": "Client-ID 645458dfa73a241"
+        }
+      };
+      var req = http.request(options, function (res) {
+        var chunks = [];
+
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+          var body = Buffer.concat(chunks);
+          if (isSevenChar) {
+            isSevenChar = false;
+            title = JSON.parse(body.toString()).data.title;
+            var tags = JSON.parse(body.toString()).data.tags;
+            var pathObj = {};
+            pathObj.title = title;
+            pathObj.pathName = pathName;
+            if (tags !== undefined) {
+              for (var i = 0; i < tags.length; i++) {
+                tags[i] = tags[i].name;
+              }
+              pathObj.tags = tags;
+            }
+            db.collection("imgur").update({pathName: pathName}, pathObj, {upsert:true});
+          }
+        });
+      });
+      req.end();
+      callback();
+    }
+  ],
+  function() {
+    done();
+  });
+});
+
+for (var i = 0; i < 500; i++) {
+  q.push({index: i});
+}
